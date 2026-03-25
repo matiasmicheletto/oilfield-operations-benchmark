9651ca8cd348a8fb3db98193da1c890cd48978e4
matias <matiasmicheletto@gmail.com>
2026-03-25 17:39:58 -0300

    Fixed zpl model template

--- generator/core/zpl_generator.py
+++ generator/core/zpl_generator.py
@@ -30,6 +30,10 @@ param maxcost := {limits.get('max_cost', 900)};
 param maxquantity := {limits.get('max_quantity', 9)};
 param crews := {res.get('crews', 1)};
 
+# Tolerances
+param gpt_inferior := 10; # Percentage
+param gpt_superior := 10; # Percentage
+
 # Variables
 var newregime[P] >= 0 <= 100;
 var z[P] binary;
@@ -49,32 +53,44 @@ subto lossbound: loss <= maxloss;
 subto costbound: cost <= maxcost;
 subto quantitybound: (sum <i> in P: z[i]) <= maxquantity;
 
-subto grossproduction: forall <k> in B:
-    sum <i> in P with i > 0 and Bat[i] == k: G[i] * newregime[i] / 100 == Gpt[k];
+# Constraints to link production to regimes and batteries
+subto grossproduction_superior: forall <k> in B:
+    sum <i> in P with i > 0 and Bat[i] == k: G[i] * newregime[i] / 100 <= Gpt[k] * (1 + gpt_superior / 100);
 
+# Constraints to link production to regimes and batteries
+subto grossproduction_inferior: forall <k> in B:
+    sum <i> in P with i > 0 and Bat[i] == k: G[i] * newregime[i] / 100 >= Gpt[k] * (1 - gpt_inferior / 100);
+
+# Constraints to link regimes to whether a well is operated or not
 subto linkzwupper: forall <i> in P with i > 0:
     newregime[i] >= R[i] - 100 * z[i];
 subto linkzwlower: forall <i> in P with i > 0:
     newregime[i] <= R[i] + 100 * z[i];
 
+# Constraints to link routing variables to whether a well is operated or not
 subto routeentry: forall <i> in P with i > 0:
     sum <j> in V: x[j,i] == z[i];
 subto routeexit: forall <i> in P with i > 0:
     sum <j> in V: x[i,j] == z[i];
 
-subto departure: sum <i> in P with i > 0: x[0,i] == crews;
-subto arrival:   sum <i> in P with i > 0: x[i,0] == crews;
+# Constraints to limit the number of routes that can be taken from and to the depot
+subto departure: sum <i> in P with i > 0: x[0,i] <= crews;
+subto arrival:   sum <i> in P with i > 0: x[i,0] <= crews;
 
+# Flow conservation constraints for the routing
 subto flow: forall <i> in P with i > 0:
     sum <j> in V: x[j,i] == sum <j> in V: x[i,j];
 
+# Constraints to link routing variables to the order of visits
 subto mtz: forall <i,j> in P*P with i != j and i > 0 and j > 0:
     y[i] + 1 <= y[j] + card(P) * (1 - x[i,j]);
 
+# Definitions of the objectives
 subto defdistance: distance == sum <i,j> in V*V: D[i,j] * x[i,j];
 subto defloss: loss == sum <i> in P: (G[i] - N[i]) * newregime[i] / 100;
 subto defcost: cost == sum <i> in P: C[i] * z[i];
 
+# Constraints to prevent routes from a node to itself
 subto nonsensex: forall <i> in V: x[i,i] == 0;
 """
         with open(output_path, "w") as f:
